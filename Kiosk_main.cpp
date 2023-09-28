a09df603573354d480db6929006ce9821a38b868
yunseo choi <a01047846070@gmail.com>
2023-09-28 17:05:36 +0900

    오류있는거 뺴로 다 합침

--- Kiosk_main.cpp
+++ Kiosk_main.cpp
@@ -1,69 +1,115 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
 #include<iostream>
 using namespace std;
 
 void sandwich_price(int n);//샌드위치 가격을 계산하는 함수
 void coffee_price(int n);//커피 가격을 계산하는 함수
 
-int main(void)//main함수 시작
-{
-	int menu, cof, sand; //정수 변수 선언
+class CafeOrderSystem { //클래스 CafeOrderSystem
+public: //공개
+	void run() { //run 함수 작성
+		int menu; //menu 변수
 
-	//메뉴 목록 출력
-	cout << "============[메뉴]============" << endl;
-	cout << " 1. 커피" << endl;
-	cout << " 2. 샌드위치" << endl;
-	cout << " 0. 종료" << endl;
-	cout << "==============================" << endl;
+		while (1) { //무한반복 함수
+			ShowMenu(); //ShowMenu()함수 부르기
+			cout << "메뉴를 선택하세요: "; //문자 출력
+			cin >> menu; //menu에 입력한 문자 넣기
 
-	//사용자로부터 메뉴 선택 받기
-	cout << "메뉴를 선택하세요: ";
-	cin >> menu;
-
-
-	while (1) // 무한 반복 코드 사용
-	{
-		if (menu == 1) // 조건식이 1로 참이면 실행
-		{
-			cout << "------------[커피]------------\n";
-			cout << " 1) 아메리카노(400)\n";
-			cout << " 2) 카페라떼 (700)\n";
-			cout << " 3) 카페라떼 (800)\n";
-			cout << " 0. 종료\n";
-			cout << "------------------------------\n\n"; // 입력으로 1을 받을 경우 나오는 선택지
-			cout << "품목을 선택하세요: "; // 입력을 받은 후 다음 
-			cin >> cof; // 커피 값 입력
-
-			coffee_price(cof); // 커피 값 저장
-			break; // break를 만나 첫 번째 조건문을 빠져나간다
-		}
-		else if (menu == 2) // 조건식이 2로 참이면 실행
-		{
-			cout << "------------[샌드위치]------------\n";
-			cout << " 1) 샌드위치 (700)\n";
-			cout << " 2) 그릴샌드위치 (800)\n";
-			cout << " 3) 라즈베리 샌드위치 (800)\n";
-			cout << " 0. 종료\n";
-			cout << "---------------------------------\n\n";
-			cout << "품목을 선택하세요: ";
-			cin >> sand; // 샌드위치 값 입력
-
-			sandwich_price(sand); // 샌드위치 값 저장
-			break; // break를 만나 두 번째 조건문을 빠져나감
-		}
-		else if (menu == 0) // 조건식이 0으로 참이면 실행
-		{
-			cout << "종료합니다." << endl;
-			break; // break를 만나 else if문을 빠져나감
+			switch (menu) { //스위치 문에 menu 넣기
+			case 1: // 1이 눌리면
+				CoffeeOrder(); //CoffeeOrder() 함수 실행
+				break; //스위치 문 나가기
+			case 2: // 2가 눌리면
+				SandwichOrder(); //SandwichOrder() 함수 실행
+				break; //스위치 문 나가기
+			case 0: //0이 눌리면
+				cout << "종료합니다." << endl; // 문자 출력
+				return;
+			default: //그 무엇도 아니라면
+				cout << "잘못된 선택입니다. 다시 선택하세요." << endl; //문자 출력
+			}
 		}
 	}
-	return 0;
-}
+
+private: //은닉
+	void ShowMenu() { //ShowMenu() 함수 선언
+		cout << "============[메뉴]============" << endl; //문자 출력
+		cout << " 1. 커피" << endl; //문자 출력
+		cout << " 2. 샌드위치" << endl; //문자 출력
+		cout << " 0. 종료" << endl; //문자 출력
+		cout << "==============================" << endl; //문자 출력
+	}
+
+	void CoffeeOrder() { //CoffeeOrder()함수 선언
+		int coffeeChoice; //coffeeChoice 변수 선언
+		cout << "------------[커피]------------\n"; //문자 출력
+		cout << " 1) 아메리카노(400)\n"; //문자 출력
+		cout << " 2) 카페라떼 (700)\n"; //문자 출력
+		cout << " 3) 카페모카 (800)\n"; //문자 출력
+		cout << " 0. 종료\n"; //문자 출력
+		cout << "------------------------------\n\n"; //문자 출력
+		cout << "커피 종류를 선택하세요: "; //문자 출력
+		cin >> coffeeChoice; //입력한 문자를 coffeeChoice에 입력
+		coffee_price(coffeeChoice); // coffee_price를 불러서 coffeeChoice 삽입
+	}
+
+	void SandwichOrder() { //SandwichOrder() 함수 선언
+		int sandwichChoice; // sandwichChoice 변수 선언
+		cout << "------------[샌드위치]------------\n"; ////문자 출력
+		cout << " 1) 샌드위치 (700)\n"; //문자 출력
+		cout << " 2) 그릴샌드위치 (800)\n"; //문자 출력
+		cout << " 3) 라즈베리 샌드위치 (800)\n"; //문자 출력
+		cout << " 0. 종료\n"; //문자 출력
+		cout << "---------------------------------\n\n"; //문자 출력
+		cout << "샌드위치 종류를 선택하세요: "; //문자 출력
+		cin >> sandwichChoice; // 입력한 문자를 sandwichChoice에 삽입
+		sandwich_price(sandwichChoice); //sandwich_price를 불러서 sandwichChoice 삽입
+	}
+};
+
+class change { //change 클래스 정의
+public: //접근 제어자 public
+	void Calculator_change(int g) { //거스름돈을 계산하고 출력하는 멤버 함수 선언
+		if (g >= 0) {
+			cout << "\n\n감사합니다! 맛있게 드세요. 잔돈은 다음과 같습니다.\n"; //감사합니다! 맛있게 드세요. 잔돈은 다음과 같습니다. 출력
+
+			cout << "오천원 " << g / 5000 << "장" << endl; //오천원 개수 출력
+			g %= 5000;
+			cout << "천원 " << g / 1000 << "장" << endl; //천원 개수 출력
+			g %= 1000;
+			cout << "오백원 " << g / 500 << "개" << endl; //오백원 개수 출력
+			g %= 500;
+			cout << "백원 " << g / 100 << "개" << endl; //백원 개수 출력
+			cout << "\n"; //줄 바꿈
+
+		}
+		else {
+			g = -g; //g에 -g를 넣어 음수였던 g를 양수로 바꿈
+			cout << g << "원이 부족합니다.\n"; //(부족한 잔액의 액수)원이 부족합니다. 출력
+
+		}
+	}
+};
+
+change ch; //change 클래스를 ch로 선언
+
+class price { //계산 클래스
+public:
+	int mony, g;  //정수방 mony와 g 생성
+	void my_mony(int m) { //my_mony 함수 생성
+		mony = m; //mony에 m 저장
+	}
+
+	void price_menu(int p) { //price_menu 함수 생성
+		g = mony - p;  //계산 결과를 g에 저장
+		ch.Calculator_change(g); //change 클래스의 Calculator_change 함수 호출
+	}
+};
+
+price pr;
 
 void coffee_price(int n) //coffee_price함수 작성
 {
-	int m, g; //정수형 변수 m, g 선언
+	int m;
 	int ame = 400, latte = 700, moca = 800; //정수형 변수 ame = 400, latte = 700, moca = 800로 선언
 
 	if (n == 0) //if문 n = 0 일때
@@ -73,49 +119,32 @@ void coffee_price(int n) //coffee_price함수 작성
 
 	while (n != 0) //n = 0 이 아닐 때
 	{
-		cout<< "금액을 투자하세요"; //금액을 투자하세요 출력
+		cout << "금액을 투자하세요"; //금액을 투자하세요 출력
 		cin >> m;
+		pr.my_mony(m); //change 클래스의 Calculator_change 함수 호출
+
 		switch (n) //switch문
 		{
 		case 1: //1이 눌렸을 때
-			g = m - ame; //m - ame의 값을 g에 저장
+			pr.price_menu(ame); //아메리카노의 값을 price_menu에 저장
 			break; //switch문 나가기
 		case 2: //2가 눌렸을 때
-			g = m - latte; //m - latte의 값을 g에 저장
+			pr.price_menu(latte); //라떼의 값을 price_menu에 저장
 			break; //switch문 나가기
 		case 3: //3이 눌렸을 때
-			g = m - moca; //m - moca의 값을 g에 저장
+			pr.price_menu(moca); //모카의 값을 price_menu에 저장
 			break; //switch문 나가기
 		default: //위의 숫자를 입력한게 아니라면
 			cout << "다시 입력하세요" << endl; // 다시 입력하세요 출력
 		}
 
-		if (g >= 0) //만약 g >= 일 때
-		{
-			cout<<"\n\n감사합니다! 맛있게 드세요. 잔돈은 다음과 같습니다.\n"; //감사합니다! 맛있게 드세요. 잔돈은 다음과 같습니다. 출력
-			
-			cout<<"오천원 "<<g<<"장"<<endl; //오천원 개수 출력
-			g %= 5000;
-			cout<<"천원 "<<g<<"장"<<endl; //천원 개수 출력
-			g %= 1000;
-			cout << "오백원 " << g << "개" << endl; //오백원 개수 출력
-			g %= 500;
-			cout << "백원 " << g << "개" << endl; //백원 개수 출력
-			cout<<"\n"; //줄 바꿈
-			break; //if문 나가기
-		}
-		else //만약 g >= 이 아니라면
-		{
-			g = -g; //g에 -g를 넣어 음수였던 g를 양수로 바꿈
-			cout<<g<<"원이 부족합니다.\n"; //(부족한 잔액의 액수)원이 부족합니다. 출력
-			continue; //다시 금액을 입력하기
-		}
+		break; //while문 나가기
 	}
 }
-//---------------이다영
+
 void sandwich_price(int n)//샌드위치의 종류와 지불한 금액을 입력받아 계한하는 함수
 {
-	int m, g; //금액m과 거스름돈g를 저장하는 변수
+	int m;
 	int sand = 700, gril = 800, ras = 800; //샌드위치의 가격
 
 	if (n == 0) //주문 종류n이 0이면 종료
@@ -127,42 +156,29 @@ void sandwich_price(int n)//샌드위치의 종류와 지불한 금액을 입력받아 계한하는 함
 	{
 		cout << "금액을 투자하세요";
 		cin >> m; //입금받을 금액을 받음
-
+		pr.my_mony(m);
 		switch (n)
 		{
 		case 1:
-			g = m - sand; // 샌드위치 가격을 차감하여 거스름돈 계산
+			pr.price_menu(sand); //샌드위치의 값을 price_menu에 저장
 			break;
 		case 2:
-			g = m - gril; // 그릴 샌드위치 가격을 차감하여 거스름돈 계산
+			pr.price_menu(gril); //그릴 샌드위치의 값을 price_menu에 저장
 			break;
 		case 3:
-			g = m - ras; // 라즈베리 샌드위치 가격을 차감하여 거스름돈 계산
+			pr.price_menu(ras); //라즈베리 샌드위치의 값을 price_menu에 저장
 			break;
 		default:
-			cout<<"다시 입력하세요"; // 유효하지 않은 주문일 경우 다시 입력 요청
+			cout << "다시 입력하세요"; // 유효하지 않은 주문일 경우 다시 입력 요청
 		}
 
-		if (g >= 0)
-		{
-			// 거스름돈을 분할하여 출력
-			cout << "\n\n감사합니다! 맛있게 드세요. 잔돈은 다음과 같습니다.\n";
-
-			cout << "오천원 " << g << "장" << endl;
-			g %= 5000;
-			cout << "천원 " << g << "장" << endl;
-			g %= 1000;
-			cout << "오백원 " << g << "개" << endl;
-			g %= 500;
-			cout << "백원 " << g << "개" << endl;
-			cout << "\n";
-			break; // 거스름돈 계산 후 루프 종료
-		}
-		else
-		{
-			g = -g; // 부족한 금액을 양수로 변환하여 표시
-			cout << g << "원이 부족합니다.\n"; // 부족한 금액을 알림
-			continue; // 고객에게 다시 금액을 입력받도록 계속 진행
-		}
+		break; //while문 나가기
 	}
+}
+
+int main(void)//main함수 시작
+{
+	CafeOrderSystem cafe; //클래스 CafeOrderSystem를 cafe로 선언
+	cafe.run(); //클래스 CafeOrderSystem에 있는 run()함수 부름
+	return 0;
 }
