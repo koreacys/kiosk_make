9a52a13189f8782b2add0525f28ec4df0b4b1913
yunseo choi <a01047846070@gmail.com>
2023-10-14 22:22:57 +0900

    뭐

--- Kiosk_main.cpp
+++ Kiosk_main.cpp
@@ -1,12 +1,130 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
+
+// 전역변수로 선언하고 초기화
+int bulgogi = 10, shrimp = 10, chicken = 10, cheese = 10, egg = 10;
+int potato = 10, nuggets = 10, cstick = 10, cpotatoes = 10;
+int cola = 10, zero_cola = 10, soda = 10, orange = 10, water = 10;
 
 void hamburger_price(int n); // 햄버거 가격을 계산하는 함수
 void side_price(int n); // 사이드 메뉴 가격을 계산하는 함수
 void drink_price(int n);// 음료 메뉴 가격을 계산하는 함수
+void setmenu_price(int n);//햄버거 세트메뉴를 계산하는 함수
 
+class SumAll { // 매출 관리 클래스 
+public:
 
-class CafeOrderSystem { //클래스 CafeOrderSystem
+    int b1 = 3500, b2 = 3000, b3 = 4000, b4 = 3700, b5 = 3200; // 햄버거 가격
+    int s1 = 1200, s2 = 1500, s3 = 1000, s4 = 2000; // 사이드 가격
+    int d1 = 1500, d2 = 1500, d3 = 1500, d4 = 1200, d5 = 1000; // 음료 가격
+    int n = 0; // 햄버거, 사이드, 음료의 개수를 받는 변수 초기화
+    int sum1 = 0, sum2 = 0, sum3 = 0; // 각 매출 합산을 받는 변수 초기화
+    int sum4 = sum1 + sum2 + sum3; // 모든 매출의 합산을 받는 변수
+
+    void ham_sale(int n) {
+        sum1 = (b1 * n) + (b2 * n) + (b3 * n) + (b4 * n) + (b5 * n);
+        cout << "햄버거 매출은 " << sum1 << " 원 입니다.";
+    }
+
+    void side_sale(int n) {
+        sum2 = (s1 * n) + (s2 * n) + (s3 * n) + (s4 * n);
+        cout << "사이드 매출은 " << sum2 << " 원 입니다.";
+    }
+
+    void drink_sale(int n) {
+        sum3 = (d1 * n) + (d2 * n) + (d3 * n) + (d4 * n) + (d5 * n);
+        cout << "음료 매출은 " << sum3 << " 원 입니다.";
+    }
+
+    void all_sale(int n) {
+        cout << "총 매출은 " << sum4 << " 원 입니다.";
+    }
+
+};
+
+SumAll SA; // 매출 관리 객체
+
+void manager() { //관리자 모드 함수
+    // 파라메터 n과 지역변수들 삭제
+    int choice; // 사용자가 선택한 메뉴 번호
+    int amount; // 재고를 추가하거나 줄일 수량
+
+    cout << "============[관리자 모드]============" << endl; // 문자 출력
+    cout << "각 메뉴의 남은 수량은 다음과 같습니다." << endl; // 문자 출력
+    cout << "1) 불고기버거: " << bulgogi << endl; // 불고기버거의 남은 수량 출력
+    cout << "2) 새우버거: " << shrimp << endl; // 새우버거의 남은 수량 출력
+    cout << "3) 치킨버거: " << chicken << endl; // 치킨버거의 남은 수량 출력
+    cout << "4) 치즈버거: " << cheese << endl; // 치즈버거의 남은 수량 출력
+    cout << "5) 에그버거: " << egg << endl; // 에그버거의 남은 수량 출력
+    cout << "6) 감자튀김: " << potato << endl; // 감자튀김의 남은 수량 출력
+    cout << "7) 치킨너겟: " << nuggets << endl; // 치킨너겟의 남은 수량 출력
+    cout << "8) 치즈스틱: " << cstick << endl; // 치즈스틱의 남은 수량 출력
+    cout << "9) 치즈감자: " << cpotatoes << endl; // 치즈감자의 남은 수량 출력
+    cout << "10) 콜라: " << cola << endl; // 콜라의 남은 수량 출력
+    cout << "11) 제로콜라: " << zero_cola << endl; // 제로콜라의 남은 수량 출력
+    cout << "12) 사이다: " << soda << endl; // 사이다의 남은 수량 출력
+    cout << "13) 오렌지주스: " << orange << endl; // 오렌지주스의 남은 수량 출력
+    cout << "14) 물: " << water << endl; // 물의 남은 수량 출력
+    cout << "==================================" << endl; // 문자 출력
+
+    while (1) { // 무한반복
+        cout << "재고를 추가하거나 줄일 메뉴 번호를 입력하세요. (0은 종료)" << endl; // 문자 출력
+        cin >> choice; // choice에 입력한 문자 넣기
+        if (choice == 0) { // choice가 0이면
+            cout << "관리자 모드를 종료합니다." << endl; // 문자 출력
+            break; // while문 나가기
+        }
+        else if (choice < 1 || choice > 14) { // choice가 1보다 작거나 14보다 크면
+            cout << "잘못된 번호입니다. 다시 입력하세요." << endl; // 문자 출력
+            continue; // while문의 처음으로 돌아가기
+        }
+        else { // choice가 1부터 14 사이면
+            cout << "재고를 추가하려면 양수, 줄이려면 음수를 입력하세요." << endl; // 문자 출력
+            cin >> amount; // amount에 입력한 문자 넣기
+            switch (choice) { // switch문에 choice 넣기
+            case 1: // choice가 1이면
+                bulgogi += amount; // 불고기버거의 남은 수량에 amount 더하기
+                break; // switch문 나가기
+            case 2: // choice가 2이면
+                shrimp += amount; // 새우버거의 남은 수량에 amount 더하기
+                break; // switch문 나가기
+            case 3: // choice가 3이면
+                chicken += amount; // 치킨버거의 남은 수량에 amount 더하기
+                break; // switch문 나가기
+            case 4: // choice가 4이면
+                cheese += amount; // 치즈버거의 남은 수량에 amount 더하기
+                break; // switch문 나가기
+            case 5: // choice가 5이면
+                egg += amount; // 에그버거의 남은 수량에 amount 더하기
+                break; // switch문 나가기
+            case 6: // choice가 6이면
+                potato += amount; // 감자튀김의 남은 수량에 amount 더하기
+                break; // switch문 나가기
+            case 7: // choice가 7이면
+                nuggets += amount; // 치킨너겟의 남은 수량에 amount 더하기
+                break; // switch문 나가기
+            case 8: // choice가 8이면
+                cstick += amount; // 치즈스틱의 남은 수량에 amount 더하기
+                break; // switch문 나가기
+            case 9: // choice가 9이면
+                cpotatoes += amount; // 치즈감자의 남은 수량에 amount 더하기
+                break; // switch문 나가기
+            case 10: // choice가 10이면
+                cola += amount; // 콜라의 남은 수량에 amount 더하기
+                break; // switch문 나가기
+            case 11: // choice가 11이면
+                zero_cola += amount; // 제로콜라의 남은 수량에 amount 더하기
+                break; // switch문 나가기
+            case 12: // choice가 12이면
+                soda += amount; // 사이다의 남은 수량에 amount 더하기
+            }
+        }
+    }
+}
+
+class HamburgerSystem { //클래스 HamburgerSystem
 public: //공개
     void run() { //run 함수 작성
         int choice, MenuChoice; //menu 변수
@@ -22,6 +140,9 @@ public: //공개
                 break; //스위치 문 나가기
             case 2: // 2가 눌리면
                 MenuOrder(); //MenuOrder() 함수 실행
+                break; //스위치 문 나가기
+            case 9: // 9가 눌리면
+                manager(); //manager() 함수 실행
                 break; //스위치 문 나가기
             case 0: //0이 눌리면
                 cout << "종료합니다." << endl; // 문자 출력
@@ -57,6 +178,10 @@ public: //공개
     }
 
 private:
+    // 선택된 메뉴와 총 가격을 저장할 변수 추가
+    vector<string> selectedMenu; // 선택된 메뉴 목록을 저장하는 벡터
+    int totalPrice = 0; // 총 가격
+
     void ShowMain() {  //ShowMenu() 함수 선언
         cout << "============[메인화면]============" << endl; //문자 출력
         cout << "1.포장" << endl;
@@ -74,8 +199,30 @@ private:
         cout << "==================================\n"; // 문자 출력
     }
 
+    void printSelectedMenu() { // 선택된 메뉴 출력 함수
+        cout << "============[선택한 메뉴]============" << endl;
+        for (const string& menu : selectedMenu) {
+            cout << menu << endl;
+        }
+        cout << "총 가격: " << totalPrice << "원" << endl;
+        cout << "=====================================" << endl;
+    }
+    void setMenu() {
+        int hambergerset;
+        cout << "=============[메뉴선택]===============" << endl;
+        cout << "1. 불고기버거세트(5500)" << endl;
+        cout << "2. 새우버거세트(5000)" << endl;
+        cout << "3. 치킨버거세트(6000) " << endl;
+        cout << "4. 치즈버거세트(5700)" << endl;
+        cout << "5. 에그버거세트(5200)" << endl;
+        cout << "===========================================" << endl;
+        cin >> hambergerset;
+        setmenu_price(hambergerset);
+    }
+
     void hamburgerMenu() {
         int hamburgerChoice;
+        int MenuType;
         cout << "============[메뉴선택]============" << endl;
         cout << "1) 불고기버거(3500)" << endl;
         cout << "2) 새우버거(3000)" << endl;
@@ -84,8 +231,50 @@ private:
         cout << "5) 에그버거(3200)" << endl;
         cout << "==================================\n";
         cin >> hamburgerChoice;
-        hamburger_price(hamburgerChoice);
+        if (hamburgerChoice != 0) {
+            switch (hamburgerChoice) {
+            case 1:
+                selectedMenu.push_back("불고기버거(3500)");
+                totalPrice += 3500;
+                break;
+            case 2:
+                selectedMenu.push_back("새우버거(3000)");
+                totalPrice += 3000;
+                break;
+            case 3:
+                selectedMenu.push_back("치킨버거(4000)");
+                totalPrice += 4000;
+                break;
+            case 4:
+                selectedMenu.push_back("치즈버거(3700)");
+                totalPrice += 3700;
+                break;
+            case 5:
+                selectedMenu.push_back("에그버거(3200)");
+                totalPrice += 3200;
+                break;
+            default:
+                cout << "다시 입력하세요" << endl;
+            }
+        }
+        cout << "=========[단품/세트 선택]============" << endl;
+        cout << "1. 단품   2. 세트" << endl;
+        cout << "===============================" << endl;
+        cin >> MenuType;
+        switch (MenuType) {
+        case 1:
+            hamburger_price(hamburgerChoice);
+            break;
+        case 2:
+            setMenu();
+        default:
+            cout << "=========================================" << endl;
+            cout << " 다시 선택해주세요" << endl;
+            cout << "=========================================" << endl;
+        }
+        printSelectedMenu();
     }
+    
 
     void sideMenu() {
         int sideMenu;
@@ -96,7 +285,29 @@ private:
         cout << "4) 치즈감자(2000)" << endl;
         cout << "==================================\n";
         cin >> sideMenu;
-        side_price(sideMenu);
+        if (sideMenu != 0) {
+            switch (sideMenu) {
+            case 1:
+                selectedMenu.push_back("감자튀김(1200)");
+                totalPrice += 1200;
+                break;
+            case 2:
+                selectedMenu.push_back("치킨너겟(1500)");
+                totalPrice += 1500;
+                break;
+            case 3:
+                selectedMenu.push_back("치즈스틱(1000)");
+                totalPrice += 1000;
+                break;
+            case 4:
+                selectedMenu.push_back("치즈감자(2000)");
+                totalPrice += 2000;
+                break;
+            default:
+                cout << "다시 입력하세요" << endl;
+            }
+        }
+        printSelectedMenu();
     }
 
     void drinkMenu() {
@@ -109,9 +320,37 @@ private:
         cout << "5) 물(1000)" << endl;
         cout << "==================================\n";
         cin >> drinkMenu;
-        drink_price(drinkMenu);
+        if (drinkMenu != 0) {
+            switch (drinkMenu) {
+            case 1:
+                selectedMenu.push_back("콜라(1500)");
+                totalPrice += 1500;
+                break;
+            case 2:
+                selectedMenu.push_back("제로콜라(1500)");
+                totalPrice += 1500;
+                break;
+            case 3:
+                selectedMenu.push_back("사이다(1500)");
+                totalPrice += 1500;
+                break;
+            case 4:
+                selectedMenu.push_back("오렌지주스(1200)");
+                totalPrice += 1200;
+                break;
+            case 5:
+                selectedMenu.push_back("물(1000)");
+                totalPrice += 1000;
+                break;
+            default:
+                cout << "다시 입력하세요" << endl;
+            }
+        }
+        printSelectedMenu();
     }
 };
+
+HamburgerSystem hb; //클래스 CafeOrderSystem를 cafe로 선언
 
 class change { //change 클래스 정의
 public: //접근 제어자 public
@@ -209,7 +448,7 @@ price pr; //price 클래스를 pr로 선언
 
 void hamburger_price(int n) //hamburger_price함수 작성
 {
-    int Bulgogi = 3500, shrimp = 3000, chicken = 4000, cheese = 3700, egg = 3200; //정수형 변수 bur1 = 3500, bur2 = 3000, bur3 = 4000, bur4 = 3700, bur5 = 3200 로 선언
+    int bulgogi = 3500, shrimp = 3000, chicken = 4000, cheese = 3700, egg = 3200; //정수형 변수 bulgogi = 3500, shrimp = 3000, chicken = 4000, cheese = 3700, egg = 3200로 선언
 
     if (n == 0) //if문 n = 0 일때
     {
@@ -221,7 +460,7 @@ void hamburger_price(int n) //hamburger_price함수 작성
         switch (n) //switch문
         {
         case 1: //1이 눌렸을 때
-            pr.price_menu(Bulgogi); //불고기버거의 값을 price_menu에 저장
+            pr.price_menu(bulgogi); //불고기버거의 값을 price_menu에 저장
             break; //switch문 나가기
         case 2: //2가 눌렸을 때
             pr.price_menu(shrimp); //새우버거의 값을 price_menu에 저장
@@ -312,11 +551,45 @@ void drink_price(int n) //drin_price함수 작성
     }
 }
 
+void setmenu_price(int n) //setmenu_price함수 작성
+{
+    int Bulgogiset = 5500, shrimpset = 5000, chickenset = 6000, cheeseset = 5700, eggset = 5200; //정수형 변수 int Bulgogiset = 5500, shrimpset = 5000, chickenset = 6000, cheeseset = 5700, eggset = 52000 로 선언
+
+    if (n == 0) //if문 n = 0 일때
+    {
+        cout << "종료" << endl; //종료 출력
+    }
+
+    while (n != 0) //n = 0 이 아닐 때
+    {
+        switch (n) //switch문
+        {
+        case 1: //1이 눌렸을 때
+            pr.price_menu(Bulgogiset); //불고기버거세트의 값을 price_menu에 저장
+            break; //switch문 나가기
+        case 2: //2가 눌렸을 때
+            pr.price_menu(shrimpset); //새우버거새트의 값을 price_menu에 저장
+            break; //switch문 나가기
+        case 3: //3이 눌렸을 때
+            pr.price_menu(chickenset); //치킨버거세트의 값을 price_menu에 저장
+            break; //switch문 나가기
+        case 4: //3이 눌렸을 때
+            pr.price_menu(cheeseset); //치즈버거세트의 값을 price_menu에 저장
+            break; //switch문 나가기
+        case 5: //3이 눌렸을 때
+            pr.price_menu(eggset); //에그버거세트의 값을 price_menu에 저장
+            break; //switch문 나가기
+        default: //위의 숫자를 입력한게 아니라면
+            cout << "다시 입력하세요" << endl; // 다시 입력하세요 출력
+        }
+
+        break; //while문 나가기
+    }
+}
 
 
 int main(void)//main함수 시작
 {
-    CafeOrderSystem cafe; //클래스 CafeOrderSystem를 cafe로 선언
-    cafe.run(); //클래스 CafeOrderSystem에 있는 run()함수 부름
+    hb.run(); //클래스 CafeOrderSystem에 있는 run()함수 부름
     return 0;
 }
